04c1ef7132b9ed8a16121443ea422fb91180fa4d
luisisidro1965 <84687575+luisisidro1965@users.noreply.github.com>
2025-06-06 03:22:12 +0000

    Prueba prj

---
+++ src/components/Anuncios.jsx
@@ -0,0 +1,23 @@
+function Anuncios() {
+  return (
+    <>
+      <h1>Para los anuncios</h1>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad minus
+          ducimus illum magni quisquam nam ab voluptatem natus cumque. Vero
+          perferendis nisi omnis beatae, nostrum nulla eos dolore accusamus
+          expedita.
+        </span>
+        <span>
+          Soluta, quia cumque sit voluptate qui ratione incidunt maxime illum
+          eum asperiores obcaecati veniam deleniti at dolorum aperiam unde
+          laudantium doloremque earum explicabo quo distinctio officia beatae
+          sapiente eveniet! Nobis!
+        </span>
+      </p>
+    </>
+  );
+}
+
+export default Anuncios;
